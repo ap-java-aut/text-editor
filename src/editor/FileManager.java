2fdb357450fb8f84f424491881324ce547e117fb
Amiro <ramezanpoor218@gmail.com>
2025-04-24 23:50:41 +0330

    add class File manger

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,65 @@
 package editor;
-
+import java.awt.*;
+import java.io.*;
+import java.util.Scanner;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileManager {
-    public static void openFile(TextEditor textEditor, JTextArea textArea) {
-        JFileChooser fileChooser = new JFileChooser();
-        if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
-        }
+    private JTextArea textEditor;
+    private File currentFile;
+
+    public FileManager (JTextArea textEditor){
+        this.textEditor = textEditor;
     }
 
-    public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
-            JFileChooser fileChooser = new JFileChooser();
-            if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+    public void openFile (){
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
+        int result = fileChooser.showOpenDialog(null);
+
+        if(result == JFileChooser.APPROVE_OPTION){
+            File selectFile = fileChooser.getSelectedFile();
+
+            try {
+                Scanner scannerFile = new Scanner(selectFile);
+
+                while ( scannerFile.hasNextLine()){
+                    System.out.println(scannerFile.nextLine());
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e.getMessage());
             }
         }
         else{
-            //TODO
+            System.out.println("File not choose");
+        }
+    }
+    // Method to save the current content to a file
+    public void saveFile(){
+        if(currentFile == null){
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
+            int result = fileChooser.showOpenDialog(null);
+
+            if(result == JFileChooser.APPROVE_OPTION){
+                currentFile = fileChooser.getSelectedFile();
+            }
+        }
+
+        try {
+            PrintStream printFile = new PrintStream(currentFile);
+            printFile.println(textEditor.getText());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e.getMessage());
         }
     }
 
-    public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+    public void newFile (){
+        textEditor.setText(""); // Clear the editor's content
+        currentFile = null; // Reset the current file reference
     }
+
+
+
 }
